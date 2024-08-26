13fcb1f7b63a1d6a12d7bce45c62ab1124f8bef7
SergeySKA_10 <karakoskinsergej@gmail.com>
2024-08-26 18:58:58 +0300

    create state and event

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,15 +1,45 @@
+import {Component} from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, surname, salary, increase}) => {
-    const increaseClass = increase ? ' increase' : '';
+export default class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            star: false
+        };
+    }
 
-    return (
-        <li className={"list-group-item d-flex justify-content-between" + increaseClass}>
-            <span className="list-group-item-label">{name} {surname}</span>
+    changeIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        })) 
+    }
+
+    addStar = () => {
+        this.setState(({star}) => ({
+            star: !star
+        }))
+    }
+
+    render() {
+        const {name, surname, salary} = this.props,
+              {increase, star} = this.state,
+              increaseClass = increase ? ' increase' : '',
+              starClass = star ? ' like' : '';
+
+        return (
+        <li className={"list-group-item d-flex justify-content-between" + increaseClass + starClass}>
+            <span 
+                className="list-group-item-label"
+                onClick={this.addStar}>
+                    {name} {surname}
+            </span>
             <input type="text" className="list-group-item-input" defaultValue={salary ? salary + "$" : '1000$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={this.changeIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -20,7 +50,6 @@ const EmployeesListItem = ({name, surname, salary, increase}) => {
                 <i className="fas fa-star"></i>
             </div>
         </li>
-    );
+        );
+    }
 }
-
-export default EmployeesListItem;
